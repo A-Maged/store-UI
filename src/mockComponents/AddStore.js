d7d57775f150e2084161d7b33faaf76e9a8e848e
A-Maged <a.maged99@outlook.com>
2017-07-16 03:26:35 +0200

    .

--- src/mockComponents/AddStore.js
+++ src/mockComponents/AddStore.js
@@ -10,10 +10,23 @@ class AddStore extends Component {
 
         this.state = {
             fireRedirect: false,
-            name: '',
-            description: '',
-            slug: '',
-            serverMsg: {},
+            store: {
+				name: 			'',
+				description: 	'',
+				location: {
+					address :	'',
+					longitude: 	0,
+					latitude: 	0
+				},
+				featuredImg:	'',
+				coverImgLink: 	'',
+				catagories: 	[],
+				hasDelivery: 	false,
+				deliveryCities: [],
+			
+				items:[]
+			}
+			
         }
 
     }
@@ -77,8 +90,9 @@ class AddStore extends Component {
 
 
     render() {
-        
-        return (
+        var store = this.state.store;
+
+		return (
             <div>
 
 
@@ -91,17 +105,57 @@ class AddStore extends Component {
                     <div>
 						<input  name="name"
 								onChange={e=>{ this.setState({name: e.target.value }) } }  
-                                value={this.state.name}   
+                                value={store.name}   
                                 type="text"  placeholder="name" />
                     </div>
-                  
+			
+
 					<textarea name="description"
 							  onChange={e=>{ this.setState({description: e.target.value }) } }  
-                              value={this.state.description} 
+                              value={store.description} 
                               placeholder="description"></textarea>
 
- 
-                    <button>add</button>
+					<div>
+						<input  name="address"
+								onChange={e=>{ this.setState({address: e.target.value }) } }  
+								value={store.location.address}   
+								type="text"  placeholder="address" />
+					</div>
+					
+					<div>
+							<input  name="longitude"
+									onChange={e=>{ this.setState({longitude: e.target.value }) } }  
+									value={store.location.longitude}   
+									type="text"  placeholder="longitude" />
+
+							<input  name="latitude"
+									onChange={e=>{ this.setState({latitude: e.target.value }) } }  
+									value={store.location.latitude}   
+									type="text"  placeholder="latitude" />
+					</div>
+			
+
+					<div>
+						<input  name="featuredImg"
+								onChange={e=>{ this.setState({featuredImg: e.target.value }) } }  
+								value={store.featuredImg}   
+								type="text"  placeholder="featuredImg" />
+					</div>
+
+					<div>
+						<input  name="coverImgLink"
+								onChange={e=>{ this.setState({coverImgLink: e.target.value }) } }  
+								value={store.coverImgLink}   
+								type="text"  placeholder="coverImgLink" />
+					</div>
+
+
+					<div>
+						hasDelivery
+						<input type="checkbox" checked={store.hasDelivery} />
+					</div>
+					
+					<button>add</button>
                 </form>
 
 
--- src/mockComponents/Root.js
+++ src/mockComponents/Root.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Stores from './Stores';
 import SingleStore from './SingleStore';
 import Item from './Item';
+import AddStore from './AddStore';
 
 const NoMatch = ({ location }) => (
 	<h3>No match for <code>{location.pathname}</code></h3>
@@ -16,12 +17,14 @@ const CmRouter = () => (
 
 			<ul>
 				<li><Link to="/">Home</Link></li>
+				<li><Link to="/addStore">add store</Link></li>
 			</ul>
 
 			<hr/>
 
 			<Switch>
 				<Route exact path="/" component={Stores}/>
+				<Route path="/addStore" component={AddStore}/>
 				<Route path="/singleStore/:storeslug/:itemId" component={Item}/>
 				<Route path="/singleStore/:storeslug" component={SingleStore}/>
 				<Route component={NoMatch}/>            
